a4d57ad5d62755456b416e48b4dfe0ebed1f8b55
Jimmy_Jiang <jimmyjiang914@gmail.com>
2019-01-07 01:34:59 -0500

    Create minimum_numbers.py
    Randomly generates 10 numbers and prints the minimum number generated

---
+++ minimum_numbers.py
@@ -0,0 +1,10 @@
+import random
+
+minimum = 100
+for i in range(10):
+    random_number = random.randint(0, 100)
+    print("The number generated is {}".format(random_number))
+    if random_number <= minimum:
+        minimum = random_number
+print(minimum)
+
