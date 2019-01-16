b998351d423b899811c03f300625073ccf0bd110
Jimmy_Jiang <jimmyjiang914@gmail.com>
2019-01-16 03:39:20 -0500

    Update Binary search algorithm.py
    Fixed bugs with edge conditions

--- Binary search algorithm.py
+++ Binary search algorithm.py
@@ -1,15 +1,16 @@
 #Assume sorted array only
 def binarySearch(arr, value):
+    import math
     left = min(range(len(arr))) #left pointer
     right = max(range(len(arr))) #right pointer
-    while left < right:
-        middle = round((right+left)/2)
+    while left <= right:
+        middle = math.floor((right + left) / 2)
         if arr[middle] == value:
             return middle
         elif arr[middle] > value:
-            right = middle
+            right = middle - 1
         elif arr[middle] < value:
-            left = middle
+            left = middle + 1
     return -1
 
-print(binarySearch([2,5,6,9,13,15,28],15)) #example input
+print(binarySearch([2,5,6,9,13,15,28,30],3)) #example input
