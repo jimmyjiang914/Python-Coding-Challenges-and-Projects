ce194e0d6a6ef3251eae1eae4dea4a43032f64cf
Jimmy_Jiang <jimmyjiang914@gmail.com>
2019-01-16 01:53:06 -0500

    Update Linear search algorithm.py
    simplified linear search algorithm

--- Linear search algorithm.py
+++ Linear search algorithm.py
@@ -1,13 +1,7 @@
 def linearSearch(array, value):
-    counter = 0
     for i in range(len(array)):
         if array[i] == value:
             return i
-            break
-        else:
-            counter += 1
-            continue
-    if counter == len(array):
-        return -1
+    return -1
 
-print(linearSearch([100],200)) #Example input
+print(linearSearch([100,300],200)) #Example input
