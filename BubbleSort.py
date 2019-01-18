2001d003d81f7f3fc0c2df8b537cb0c24f126d3f
Jimmy_Jiang <jimmyjiang914@gmail.com>
2019-01-17 23:59:30 -0500

    Update BubbleSort.py
    optimized for almost sorted arrays.

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,11 +1,14 @@
 def bubblesort(arr):
     for i in range(len(arr),1, -1):
+        swaps = 0
         for j in range(0,i-1):
             if arr[j] > arr[j+1]:
                 temp = arr[j+1]
                 arr[j+1] = arr[j]
                 arr[j] = temp
-
+                swaps += 1
+        if swaps == 0:
+            return arr
     return arr
 
 print(bubblesort([46,53,86,4,2]))
